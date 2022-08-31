2bfe3a2339fd10b67c2925ca6f15d7b6ac56f572
Olga <97553920+OlgaOvchinnikova@users.noreply.github.com>
2022-08-31 21:33:32 +0300

    Create les8_Clouds.java

---
+++ les8_Clouds.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.*;
+
+public class Clouds{
+    Integer all;
+
+    public Clouds() {
+    }
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
