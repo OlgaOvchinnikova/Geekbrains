74ca046cf68fb2b26052eea5660ec0d9e79f2375
Olga <97553920+OlgaOvchinnikova@users.noreply.github.com>
2022-08-31 21:36:11 +0300

    Create les8_DBRow.java

---
+++ les8_DBRow.java
@@ -0,0 +1,46 @@
+package model;
+
+public class DBRow {
+    // запись в базу данных
+    private String city;
+    private String localDate;
+    private String weatherText;
+    private Double temperature;
+
+    public DBRow(String city, String localDate, String weatherText, Double temperature) {
+        this.city = city;
+        this.localDate = localDate;
+        this.weatherText = weatherText;
+        this.temperature = temperature;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getLocalDate() {
+        return localDate;
+    }
+
+    public String getWeatherText() {
+        return weatherText;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return  "Город='" + city + '\'' +
+                ", дата='" + localDate + '\'' +
+                ", погода='" + weatherText + '\'' +
+                ", температура=" + temperature +
+                '}';
+    }
+
+    public String toCuteString() {
+        return  localDate + '\t' +
+                " — " + weatherText + ",\t" +
+                "t°C = " + temperature;
+    }
