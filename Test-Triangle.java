17025754d29f195b0d4f03b4c523a3190f57b26e
Olga <97553920+OlgaOvchinnikova@users.noreply.github.com>
2022-09-29 22:31:37 +0300

    Create Test-Triangle.java

---
+++ Test-Triangle.java
@@ -0,0 +1,40 @@
+//package geekbrains.webui.lesson_4;
+//
+//import org.junit.jupiter.api.*;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import org.junit.platform.commons.logging.LoggerFactory;
+//
+//import java.util.logging.Logger;
+//
+//public class TriangleAreaTest {
+//
+//    static Logger logger = (Logger) LoggerFactory.getLogger(TriangleAreaTest.class);
+//
+//    @Test
+//    @DisplayName("Zero check")
+//    @Order(1)
+//    @RepeatedTest(1)
+//    @Tag("Triangle_area")
+//    public void zeroCheck()
+//    {
+//        triangleArea.a
+//        logger.info("Zero check logs");
+//        System.out.println("Zero check");
+//    }
+//
+////    @ParameterizedTest
+////    @ValueSource(int = {int a, int b, int c})
+////    @DisplayName("Int check")
+////    @Order(2)
+////    @RepeatedTest(1)
+////    @Tag("Triangle_area")
+////    public void intCheck(int[] args)
+////    {
+////        System.out.println("Zero check");
+////    }
+//
+//
+//
+//
+//}
